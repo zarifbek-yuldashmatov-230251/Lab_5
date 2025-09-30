573faaed0398e8001b26e6613f3f3e2f95752c39
zarifbek-yuldashmatov-230251 <161605979+zarifbek-yuldashmatov-230251@users.noreply.github.com>
2025-09-30 14:35:37 +0500

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,12 +1,40 @@
 #include <iostream>
 #include <string>
-using namespace std;
-#include "Const1.h"
-int main() {
-Person eleven("Husan", 20);
-eleven.introduce();
-    eleven.haveBirthday();
-Student Husan("Husan", 20, "New Uzbekistan University");
-    Husan.study();
-    return 0;
-}
+#ifndef CONST1_H
+#define CONST1_H
+class Person {
+    public:
+    string name = "N/A";
+    int age = 0;
+    Person(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+    void introduce() {
+        cout<<"My name is "<<name<<" and I'm "<<age<<" years old."<<endl;
+    }
+    void haveBirthday() {
+        age+=1;
+        cout<<"My age is "<<age<<" years old."<<endl;
+    }
+    ~Person() {
+        cout<<"Bye bye!"<<endl;
+    }
+};
+    class Student:public Person {
+    private:
+        string university;
+    public:
+        Student(string name, int age, string university):Person(name,age) {
+            this->university = university;
+            cout<<"Confirmed"<<endl;
+        }
+            void study() {
+                cout<<name<<" is studying at "<<university<<"."<<endl;
+            }
+
+
+    };
+
+#endif //CONST1_H
+
